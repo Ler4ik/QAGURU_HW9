0cf21fc225d28f59e2af836c919439f919c8d024
sanzharova <valvalsan@gmail.com>
2023-04-01 00:53:47 +0700

    added test2

--- src/test/java/OtusTests.java
+++ src/test/java/OtusTests.java
@@ -1,9 +1,9 @@
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.checked;
@@ -20,8 +20,7 @@ public class OtusTests {
     @ValueSource(
             strings = {"Аналитика", "Тестирование"}
     )
-
-    @ParameterizedTest(name = "test {0} test {0}")
+    @ParameterizedTest(name = "При переходе в пункт меню {0} в каталоге выбран фильтр {0}")
     public void test(String title){
         $("#categories_id").$("a[title = " + title +"]").click();
         $$("label").findBy(text(title)).preceding(0).$("input[type = checkbox]").shouldBe(checked);
@@ -31,4 +30,15 @@ public class OtusTests {
     public void closeWindow(){
         Selenide.closeWindow();
     }
+
+    @CsvFileSource(resources = "/TestData.csv")
+    @ParameterizedTest(name = "При выборе уровня {0} в разделе тестирования отображается курс {1}")
+    public void test2(String level, String courseName){
+        $("#categories_id").$("a[title = Тестирование]").click();
+        $$("p").findBy(text("Уровень")).parent().sibling(0)
+                .$$("label").findBy(text(level)).preceding(0).$("input[type = checkbox]").click();
+        $("#__next").shouldBe(text(courseName));
+    }
 }
+
+
